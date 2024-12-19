d6508a00aecad653ef9b36276b894475cd1ea297
Boone B Gorges <boonebgorges@gmail.com>
2024-12-19 12:17:46 -0600

    Delay bubble initialization by a second.
    This gives other scripts a chance to render their elements before
    inline-comments determines bubble positions.

--- frontend/class-wp.php
+++ frontend/class-wp.php
@@ -23,26 +23,29 @@ class INCOM_WordPress extends INCOM_Frontend {
 		?>
 		<script>
 		(function ( $ ) {
-			var icTimer;
-
 			$(window).on( "load", function() {
-				incom.init({
-					canComment: <?php echo parent::can_comment() == "" ? "false" : "true"; ?>,
-					selectors: '<?php echo get_option("multiselector") == "" ? $default_selectors : get_option("multiselector"); ?>',
-					moveSiteSelector: '<?php echo get_option("moveselector") == "" ? "body" : esc_js(get_option("moveselector")); ?>',
-			    countStatic: <?php echo get_option("incom_bubble_static") == "1" ? "false" : "true"; ?>,
-			    alwaysStatic: <?php echo get_option("incom_bubble_static_always") == "1" ? "true" : "false"; ?>,
-			    bubbleStyle: '<?php echo get_option("select_bubble_style") == "" ? "bubble" : esc_js(get_option("select_bubble_style")); ?>',
-			    bubbleAnimationIn: '<?php echo get_option("select_bubble_fadein") == "" ? "default" : esc_js(get_option("select_bubble_fadein")); ?>',
-			    bubbleAnimationOut: '<?php echo get_option("select_bubble_fadeout") == "" ? "default" : esc_js(get_option("select_bubble_fadeout")); ?>',
-				  // defaultBubbleText: '+',
-			    // highlighted: false,
-			    position: '<?php echo get_option("incom_select_align") == "" ? "right" : esc_js(get_option("incom_select_align")); ?>',
-			    background: '<?php echo get_option("set_bgcolour") == "" ? "#fff" : esc_js(get_option("set_bgcolour")); ?>',
-					backgroundOpacity: '<?php echo get_option("incom_set_bgopacity") == "" ? "1" : esc_js(get_option("incom_set_bgopacity")); ?>',
-					displayBranding: <?php echo get_option("incom_attribute") == "link" ? "true" : "false"; ?>,
-					<?php do_action( 'incom_wp_set_options' ); ?>
-				});
+				setTimeout(
+					function() {
+						incom.init({
+							canComment: <?php echo parent::can_comment() == "" ? "false" : "true"; ?>,
+							selectors: '<?php echo get_option("multiselector") == "" ? $default_selectors : get_option("multiselector"); ?>',
+							moveSiteSelector: '<?php echo get_option("moveselector") == "" ? "body" : esc_js(get_option("moveselector")); ?>',
+						countStatic: <?php echo get_option("incom_bubble_static") == "1" ? "false" : "true"; ?>,
+						alwaysStatic: <?php echo get_option("incom_bubble_static_always") == "1" ? "true" : "false"; ?>,
+						bubbleStyle: '<?php echo get_option("select_bubble_style") == "" ? "bubble" : esc_js(get_option("select_bubble_style")); ?>',
+						bubbleAnimationIn: '<?php echo get_option("select_bubble_fadein") == "" ? "default" : esc_js(get_option("select_bubble_fadein")); ?>',
+						bubbleAnimationOut: '<?php echo get_option("select_bubble_fadeout") == "" ? "default" : esc_js(get_option("select_bubble_fadeout")); ?>',
+						  // defaultBubbleText: '+',
+						// highlighted: false,
+						position: '<?php echo get_option("incom_select_align") == "" ? "right" : esc_js(get_option("incom_select_align")); ?>',
+						background: '<?php echo get_option("set_bgcolour") == "" ? "#fff" : esc_js(get_option("set_bgcolour")); ?>',
+							backgroundOpacity: '<?php echo get_option("incom_set_bgopacity") == "" ? "1" : esc_js(get_option("incom_set_bgopacity")); ?>',
+							displayBranding: <?php echo get_option("incom_attribute") == "link" ? "true" : "false"; ?>,
+							<?php do_action( 'incom_wp_set_options' ); ?>
+						});
+					},
+					1000
+				);
 			});
 		})(jQuery);
 		</script>
